07b8ad51bf73b12c859f8eb086d6722736596f65
knightfury16 <suhaibknight@gmail.com>
2025-05-22 18:34:26 +0600

    Added Smtp Email Sender
    This is a crude implementation. Need to refine it.

---
+++ EmailSenderLib/SmtpEmailSender/SmtpEmailSender.cs
@@ -0,0 +1,249 @@
+using System.Net.Mail;
+using EmailSenderLib.Models;
+using EmailSenderLib.TemplateRenderer;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace EmailSenderLib.SmtpEmailSender;
+
+
+
+public class SmtpEmailSender : IEmailSender, IDisposable
+{
+    private readonly SmtpEmailSettings _settings;
+    private readonly ILogger<SmtpEmailSettings> _logger;
+    private readonly ITemplateRenderer _templateRenderer;
+    private bool _disposed;
+
+    public SmtpEmailSender(
+        IOptions<SmtpEmailSettings> settings,
+        ILogger<SmtpEmailSettings> logger,
+        ITemplateRenderer templateRenderer
+    )
+    {
+        _settings = settings?.Value ?? throw new ArgumentNullException(nameof(settings));
+        _logger = logger;
+        _templateRenderer = templateRenderer;
+    }
+
+    public async Task<EmailSendResponse> SendEmailAsync(
+        EmailSendRequest request,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        try
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            _logger?.LogDebug($"Preparing to send email with subject {request.Subject}");
+
+            using var message = CreateMailMessage(request);
+            using var smtpClient = CreateSmtpClient();
+
+            await smtpClient.SendMailAsync(message, cancellationToken);
+
+            return EmailSendResponse.Success();
+        }
+        catch (System.Exception ex)
+        {
+            return EmailSendResponse.Failure(ex.Message);
+        }
+    }
+
+    public async Task<EmailSendResponse> SendTemplatedEmailAsync(
+        TemplatedEmailRequest request,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        if (_templateRenderer == null)
+        {
+            _logger?.LogError(
+                "Template renderer is not configured. Unable to send templated email."
+            );
+            return EmailSendResponse.Failure("Template Renderer is not configured.");
+        }
+        try
+        {
+            var renderedTemplate = await _templateRenderer.RenderTemplateAsync(
+                request.TemplateId,
+                request.TemplateContent,
+                cancellationToken
+            );
+
+            var emailRequest = new EmailSendRequest()
+            {
+                To = request.To,
+                From = request.From,
+                Cc = request.Cc,
+                Bcc = request.Bcc,
+                Subject = request.Subject,
+                HtmlContent = renderedTemplate.HtmlContent,
+                TextContent = renderedTemplate.TextContent,
+                Attachments = request.Attachments,
+                Priority = request.Priority,
+                Headers = request.Headers,
+            };
+
+            return await SendEmailAsync(emailRequest, cancellationToken);
+        }
+        catch (System.Exception ex)
+        {
+            return EmailSendResponse.Failure(ex.Message);
+        }
+    }
+
+    public async Task<IEnumerable<EmailSendResponse>> SendBulkEmailAsync(
+        ICollection<EmailSendRequest> requests,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(requests);
+
+        var responses = new List<EmailSendResponse>();
+
+        foreach (var request in requests)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                break;
+            }
+            var response = await SendEmailAsync(request, cancellationToken);
+            responses.Add(response);
+        }
+
+        return responses;
+    }
+
+    public async Task<IEnumerable<EmailSendResponse>> SendBulkTemplatedEmailAsync(
+        ICollection<TemplatedEmailRequest> requests,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(requests);
+
+        var responses = new List<EmailSendResponse>();
+
+        foreach (var request in requests)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                break;
+            }
+            var response = await SendTemplatedEmailAsync(request, cancellationToken);
+            responses.Add(response);
+        }
+        return responses;
+    }
+
+    protected virtual SmtpClient CreateSmtpClient()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected virtual MailMessage CreateMailMessage(EmailSendRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var message = new MailMessage();
+
+        // Set the from address
+        var from = GetFrom(request.From);
+        message.From = from;
+
+        // Set the to addresses
+        var to = request.To.Select(t => t.ToSystemMailAddress()).ToList();
+        foreach (var t in to)
+        {
+            message.To.Add(t);
+        }
+
+        // Set the cc addresses
+        var cc = request.Cc.Select(c => c.ToSystemMailAddress()).ToList();
+        foreach (var c in cc)
+        {
+            message.CC.Add(c);
+        }
+
+        // Set the bcc addresses
+        var bcc = request.Bcc.Select(b => b.ToSystemMailAddress()).ToList();
+        foreach (var b in bcc)
+        {
+            message.Bcc.Add(b);
+        }
+
+        // Set the subject
+        message.Subject = request.Subject;
+
+        // Set the body
+        // throw error if no body is provided
+        if (string.IsNullOrEmpty(request.HtmlContent) && string.IsNullOrEmpty(request.TextContent))
+        {
+            throw new ArgumentException("Either html content or text content must be provided.");
+        }
+        message.Body = request.HtmlContent ?? request.TextContent;
+        if (request.HtmlContent != null)
+        {
+            message.IsBodyHtml = true;
+        }
+
+        // Set the attachments
+        foreach (var attachment in request.Attachments)
+        {
+            message.Attachments.Add(attachment.ToSystemMailAttachment());
+        }
+
+        // Set the priority
+        message.Priority = ConverPriority(request.Priority);
+
+        // Set the headers
+        foreach (var header in request.Headers)
+        {
+            message.Headers.Add(header.Key, header.Value);
+        }
+
+        // Set the date
+        message.Headers.Add("Date", DateTime.UtcNow.ToString("r"));
+
+        return message;
+    }
+
+    private MailPriority ConverPriority(EmailPriority priority)
+    {
+        return priority switch
+        {
+            EmailPriority.Low => MailPriority.Low,
+            EmailPriority.Normal => MailPriority.Normal,
+            EmailPriority.High => MailPriority.High,
+            _ => MailPriority.Normal,
+        };
+    }
+
+    private System.Net.Mail.MailAddress GetFrom(EmailAddress? from)
+    {
+        if (from != null)
+        {
+            return from.ToSystemMailAddress();
+        }
+        return GetSenderFromSettings();
+    }
+
+    private System.Net.Mail.MailAddress GetSenderFromSettings()
+    {
+        if (string.IsNullOrEmpty(_settings.SenderEmail))
+        {
+            throw new InvalidOperationException("Sender email is not configured.");
+        }
+        return string.IsNullOrEmpty(_settings.SenderName)
+            ? new System.Net.Mail.MailAddress(_settings.SenderEmail!)
+            : new System.Net.Mail.MailAddress(_settings.SenderEmail!, _settings.SenderName!);
+    }
+
+    public void Dispose()
+    {
+        // Nothing to dispose
+        GC.SuppressFinalize(this);
+    }
+}
