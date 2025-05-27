f1ae73a8e6e252c3e5b61d1ba761f67fe57ca8ef
knightfury16 <suhaibknight@gmail.com>
2025-05-27 22:45:16 +0600

    Added default smtp port

--- EmailSenderLib/SmtpEmailSender/SmtpEmailSettings.cs
+++ EmailSenderLib/SmtpEmailSender/SmtpEmailSettings.cs
@@ -5,6 +5,6 @@ namespace EmailSenderLib.SmtpEmailSender;
 public sealed class SmtpEmailSettings : EmailSettings
 {
     public string? SmtpServer { get; set; }
-    public int SmtpPort { get; set; }
+    public int SmtpPort { get; set; } = 587; //default smtp port
     public bool EnableSsl { get; set; }
 }
