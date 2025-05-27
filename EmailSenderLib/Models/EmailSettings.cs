e83dbd92ae305401cb5ed2efed3c4d0ad7303cb6
knightfury16 <suhaibknight@gmail.com>
2025-05-27 22:44:19 +0600

    Added Domain Property in EmailSettings

--- EmailSenderLib/Models/EmailSettings.cs
+++ EmailSenderLib/Models/EmailSettings.cs
@@ -6,5 +6,6 @@ public abstract class EmailSettings
     public string? Password { get; set; }
     public string? SenderEmail { get; set; }
     public string? SenderName { get; set; }
+    public string? Domain { get; set; }
     public int TimeoutMilliseconds { get; set; } = 100000;
 }
