8d0d01666d8b9ccf0ee8ea2d4c11a13a406225ed
knightfury16 <suhaibknight@gmail.com>
2025-06-01 22:55:44 +0600

    Extended template renderer interface

--- EmailSenderLib/TemplateRenderer/ITemplateRenderer.cs
+++ EmailSenderLib/TemplateRenderer/ITemplateRenderer.cs
@@ -2,7 +2,6 @@ using EmailSenderLib.Models;
 
 namespace EmailSenderLib.TemplateRenderer;
 
-
 public interface ITemplateRenderer
 {
     /// <summary>
@@ -13,8 +12,18 @@ public interface ITemplateRenderer
     /// <param name="cancellationToken">Token to cancel the operation.</param>
     /// <returns>The rendered template content.</returns>
     public Task<RenderedContent> RenderTemplateAsync(
-        string templateId,
         object data,
+        string? templateId,
+        CancellationToken cancellationToken = default
+    );
+
+    public Task<RenderedContent> RenderTemplateAsync(
+        object data,
+        CancellationToken cancellationToken = default
+    );
+
+    public Task<RenderedContent> RenderTemplateAsync(
+        string templateId,
         CancellationToken cancellationToken = default
     );
 }
