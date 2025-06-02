76fdb00a0482833244b23f0bed3c0b06afc79fcd
knightfury16 <suhaibknight@gmail.com>
2025-06-02 23:13:51 +0600

    template email request test update on model change

--- EmailSenderTests/TemplatedEmailRequestTests.cs
+++ EmailSenderTests/TemplatedEmailRequestTests.cs
@@ -5,17 +5,25 @@ namespace EmailSenderTests;
 public class TemplatedEmailRequestTests
 {
     [Fact]
-    public void TemplateId_CannotBeNull()
+    public void TemplateContent_CannotBeNull()
     {
-        var request = new TemplatedEmailRequest(new List<EmailAddress> { new("a@example.com") });
-        Assert.Throws<ArgumentNullException>(() => request.TemplateId = null!);
+        Assert.Throws<ArgumentNullException>(
+            () =>
+                new TemplatedEmailRequest(new EmailAddress("test@gmail.com"))
+                {
+                    TemplateContent = null!,
+                }
+        );
     }
 
     [Fact]
     public void SetTemplate_SetsProperties()
     {
-        var request = new TemplatedEmailRequest(new List<EmailAddress> { new("a@example.com") });
-        request.SetTemplate("template1", new { Name = "Alice" });
+        var request = new TemplatedEmailRequest(new EmailAddress("a@example.com"))
+        {
+            TemplateContent = "testContent",
+        };
+        request.SetTemplate(new { Name = "Alice" }, "template1");
         Assert.Equal("template1", request.TemplateId);
         Assert.NotNull(request.TemplateContent);
     }
